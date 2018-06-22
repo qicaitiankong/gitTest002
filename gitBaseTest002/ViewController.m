0b0a9cc4c8ebdb74fb58209a256dc186a3e032b7
qicaitiankong <zihaoshuhan@163.com>
2018-06-22 10:40:38 +0800

    Update ViewController.m

--- gitBaseTest002/ViewController.m
+++ gitBaseTest002/ViewController.m
@@ -18,7 +18,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
+- (void)test{
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
